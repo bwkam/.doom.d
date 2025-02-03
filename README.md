257ca4967e428d5d8efb6a5ed949b9a87d9c0967
bwkam <userdev987@gmail.com>
2025-02-03 21:10:50 +0200

    readme and custom.el file

---
+++ README.md
@@ -0,0 +1,3 @@
+# my doom config
+
+yessir
---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("38c0c668d8ac3841cb9608522ca116067177c92feeabc6f002a27249976d7434" "828719a11ad6e51e5d55aef46db968e4d56c5da4513e7ce15da1a0783b1a39de" "2224b07a70ad0e4f11eef482f9551efa57d7cc27c943ffd43edd01ed85ed2ebf" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
